db1130fa09875def064c6e405164e8c9222ae9b4
Gustavolrsc <gustavolrsc@gmail.com>
2025-02-11 11:00:08 -0300

    Exception personalizada para objeto não encontrado!

--- src/main/java/com/gustavo/bookstore/service/CategoriaService.java
+++ src/main/java/com/gustavo/bookstore/service/CategoriaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gustavo.bookstore.domain.Categoria;
+import com.gustavo.bookstore.exceptions.ObjectNotFoundException;
 import com.gustavo.bookstore.repostories.CategoriaRepository;
 
 @Service
@@ -16,6 +17,6 @@ public class CategoriaService {
 	
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
