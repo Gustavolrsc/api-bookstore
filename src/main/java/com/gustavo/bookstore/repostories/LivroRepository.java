1353c44a77bb48391304d57f9d7144d39d5baf22
Gustavolrsc <gustavolrsc@gmail.com>
2025-02-13 16:52:45 -0300

    POST para Categoria

--- src/main/java/com/gustavo/bookstore/repostories/LivroRepository.java
+++ src/main/java/com/gustavo/bookstore/repostories/LivroRepository.java
@@ -1,16 +1,7 @@
 package com.gustavo.bookstore.repostories;
-
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.stereotype.Repository;
-
-
-
 import com.gustavo.bookstore.domain.Livro;
-
-
 
 @Repository
 
