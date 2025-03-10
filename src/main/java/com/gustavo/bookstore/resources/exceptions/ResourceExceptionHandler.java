7782e4564f0d59982b50dc6cf9fe3e4bb2c3dd92
Gustavolrsc <gustavolrsc@gmail.com>
2025-03-10 15:26:00 -0300

    Ajustando bug no properties test

--- src/main/java/com/gustavo/bookstore/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/gustavo/bookstore/resources/exceptions/ResourceExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.gustavo.bookstore.resources.exceptions;
 
-import java.util.Iterator;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
--- src/main/java/com/gustavo/bookstore/service/DBService.java
+++ src/main/java/com/gustavo/bookstore/service/DBService.java
@@ -18,21 +18,25 @@ public class DBService {
 	private LivroRepository livroRepository;
 
 	public void instanciaBaseDeDados() {
-		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
-		Categoria cat2 = new Categoria(null, "Ficção Científica", "Livros de ficção científica");
-		Categoria cat3 = new Categoria(null, "Autoajuda", "Livros de desenvolvimento pessoal");
+	    if (categoriaRepository.count() > 0) { 
+	        return; 
+	    }
 
-		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
-		Livro l2 = new Livro(null, "Código Limpo", "Robert Martin", "Descrição do livro", cat1);
-		Livro l3 = new Livro(null, "O Hobbit", "J.R.R. Tolkien", "Descrição do livro", cat2);
-		Livro l4 = new Livro(null, "1984", "George Orwell", "Descrição do livro", cat2);
-		Livro l5 = new Livro(null, "O Poder do Hábito", "Charles Duhigg", "Descrição do livro", cat2);
+	    Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
+	    Categoria cat2 = new Categoria(null, "Ficção Científica", "Livros de ficção científica");
+	    Categoria cat3 = new Categoria(null, "Autoajuda", "Livros de desenvolvimento pessoal");
 
-		cat1.getLivros().addAll(Arrays.asList(l1, l2));
-		cat2.getLivros().addAll(Arrays.asList(l3, l4, l5));
+	    Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
+	    Livro l2 = new Livro(null, "Código Limpo", "Robert Martin", "Descrição do livro", cat1);
+	    Livro l3 = new Livro(null, "O Hobbit", "J.R.R. Tolkien", "Descrição do livro", cat2);
+	    Livro l4 = new Livro(null, "1984", "George Orwell", "Descrição do livro", cat2);
+	    Livro l5 = new Livro(null, "O Poder do Hábito", "Charles Duhigg", "Descrição do livro", cat2);
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
+	    cat1.getLivros().addAll(Arrays.asList(l1, l2));
+	    cat2.getLivros().addAll(Arrays.asList(l3, l4, l5));
+
+	    categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+	    livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
 	}
   } 
 
